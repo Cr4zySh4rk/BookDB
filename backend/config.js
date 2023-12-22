365e8135c53d427f79e189ec5801efb925a94a1b
Adithya Manjunath <adithyam211@gmail.com>
2023-12-22 23:01:47 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@cluster0.f8mtppf.mongodb.net/?retryWrites=true&w=majority';
+  '<your mongo db key here>';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
