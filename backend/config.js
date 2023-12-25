3e16792bd5736d9a70fa59eb393eca88084d50fc
Adithya Manjunath <adithya@Adithyas-MacBook-Air.local>
2023-12-26 00:39:25 +0530

    update faker

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  '<your_mongo_db_credentials>';
+  'mongodb+srv://root:root@cluster0.f8mtppf.mongodb.net/?retryWrites=true&w=majority';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
--- faker/index.js
+++ faker/index.js
@@ -1,4 +1,52 @@
-// since we are requiring the top level of faker, load all locales by default
-var Faker = require('./lib');
-var faker = new Faker({ locales: require('./lib/locales') });
-module['exports'] = faker;
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+// Replace 'your_database_url' with the actual URL of your MongoDB database
+mongoose.connect("mongodb+srv://root:root@cluster0.f8mtppf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+    issued: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+async function generateRandomData() {
+  const books = [];
+
+  for (let i = 0; i < 13567; i++) {
+    const book = {
+      title: faker.lorem.words(),
+      author: faker.name.findName(),
+      publishYear: faker.random.number({ min: 1800, max: 2023 }), // Adjust the range as needed
+      issued: faker.random.arrayElement(['Yes', 'No']),
+    };
+
+    books.push(book);
+  }
+
+  await Book.insertMany(books);
+  console.log('Random entries inserted successfully.');
+  mongoose.connection.close();
+}
+
+generateRandomData();
